5aebb612a438cda4f5e5c757ca4774c3dd9c4b28
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-26 01:16:37 +0530

    Refactor PHP
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- Sports World/shopping/payment-method.php
+++ Sports World/shopping/payment-method.php
@@ -1,36 +1,32 @@
-<?php 
+<?php
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:login.php');
-}
-else{
+if (strlen($_SESSION['login']) == 0) {
+	header('location:login.php');
+} else {
 	if (isset($_POST['submit'])) {
-
-		mysqli_query($con,"update orders set 	paymentMethod='".$_POST['paymethod']."' where userId='".$_SESSION['id']."' and paymentMethod is null ");
+		mysqli_query($con, "update orders set 	paymentMethod='" . $_POST['paymethod'] . "' where userId='" . $_SESSION['id'] . "' and paymentMethod is null ");
 		unset($_SESSION['cart']);
 		header('location:order-history.php');
-
 	}
-?>
-<!DOCTYPE html>
-<html lang="en">
+	?>
+	<!DOCTYPE html>
+	<html lang="en">
+
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<meta name="description" content="">
 		<meta name="author" content="">
-	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
-	    <meta name="robots" content="all">
-
-	    <title>Shopping Portal | Payment Method</title>
-	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	    <link rel="stylesheet" href="assets/css/main.css">
-	    <link rel="stylesheet" href="assets/css/green.css">
-	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<meta name="keywords" content="MediaCenter, Template, eCommerce">
+		<meta name="robots" content="all">
+		<title>Shopping Portal | Payment Method</title>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/main.css">
+		<link rel="stylesheet" href="assets/css/green.css">
+		<link rel="stylesheet" href="assets/css/owl.carousel.css">
 		<link rel="stylesheet" href="assets/css/owl.transitions.css">
 		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
 		<link href="assets/css/lightbox.css" rel="stylesheet">
@@ -47,106 +43,85 @@ else{
 		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
 		<link rel="shortcut icon" href="assets/images/favicon.ico">
 	</head>
-    <body class="cnt-home">
-	
-		
-<header class="header-style-1">
-<?php include('includes/top-header.php');?>
-<?php include('includes/main-header.php');?>
-<?php include('includes/menu-bar.php');?>
-</header>
-<div class="breadcrumb">
-	<div class="container">
-		<div class="breadcrumb-inner">
-			<ul class="list-inline list-unstyled">
-				<li><a href="home.html">Home</a></li>
-				<li class='active'>Payment Method</li>
-			</ul>
-		</div>
-	</div> 
-</div>
 
-<div class="body-content outer-top-bd">
-	<div class="container">
-		<div class="checkout-box faq-page inner-bottom-sm">
-			<div class="row">
-				<div class="col-md-12">
-					<h2>Choose Payment Method</h2>
-					<div class="panel-group checkout-steps" id="accordion">
-<div class="panel panel-default checkout-step-01">
-
-		<div class="panel-heading">
-    	<h4 class="unicase-checkout-title">
-	        <a data-toggle="collapse" class="" data-parent="#accordion" href="#collapseOne">
-	         Select your Payment Method
-	        </a>
-	     </h4>
-    </div>
-
-	<div id="collapseOne" class="panel-collapse collapse in">
-
-	    <div class="panel-body">
-	    <form name="payment" method="post">
-	    <input type="radio" name="paymethod" value="COD" checked="checked"> COD
-	     <input type="radio" name="paymethod" value="Internet Banking"> Internet Banking
-	     <input type="radio" name="paymethod" value="Debit / Credit card"> Debit / Credit card <br /><br />
-	     <input type="submit" value="submit" name="submit" class="btn btn-primary">
-	    	
-
-	    </form>		
-		</div>
-
-	</div>
-</div>
-
-					  
-					  	
-					</div>
+	<body class="cnt-home">
+		<header class="header-style-1">
+			<?php include('includes/top-header.php'); ?>
+			<?php include('includes/main-header.php'); ?>
+			<?php include('includes/menu-bar.php'); ?>
+		</header>
+		<div class="breadcrumb">
+			<div class="container">
+				<div class="breadcrumb-inner">
+					<ul class="list-inline list-unstyled">
+						<li><a href="home.html">Home</a></li>
+						<li class='active'>Payment Method</li>
+					</ul>
 				</div>
 			</div>
 		</div>
-		
-<?php echo include('includes/brands-slider.php');?>
-</div>
-</div>
-<?php include('includes/footer.php');?>
-	<script src="assets/js/jquery-1.11.1.min.js"></script>
-	
-	<script src="assets/js/bootstrap.min.js"></script>
-	
-	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
-	<script src="assets/js/owl.carousel.min.js"></script>
-	
-	<script src="assets/js/echo.min.js"></script>
-	<script src="assets/js/jquery.easing-1.3.min.js"></script>
-	<script src="assets/js/bootstrap-slider.min.js"></script>
-    <script src="assets/js/jquery.rateit.min.js"></script>
-    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
-    <script src="assets/js/bootstrap-select.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-	<script src="assets/js/scripts.js"></script>
-
-	
-	
-	<script src="switchstylesheet/switchstylesheet.js"></script>
-	
-	<script>
-		$(document).ready(function(){ 
-			$(".changecolor").switchstylesheet( { seperator:"color"} );
-			$('.show-theme-options').click(function(){
-				$(this).parent().toggleClass('open');
-				return false;
+		<div class="body-content outer-top-bd">
+			<div class="container">
+				<div class="checkout-box faq-page inner-bottom-sm">
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Choose Payment Method</h2>
+							<div class="panel-group checkout-steps" id="accordion">
+								<div class="panel panel-default checkout-step-01">
+									<div class="panel-heading">
+										<h4 class="unicase-checkout-title">
+											<a data-toggle="collapse" class="" data-parent="#accordion" href="#collapseOne">
+												Select your Payment Method
+											</a>
+										</h4>
+									</div>
+									<div id="collapseOne" class="panel-collapse collapse in">
+										<div class="panel-body">
+											<form name="payment" method="post">
+												<input type="radio" name="paymethod" value="COD" checked="checked"> COD
+												<input type="radio" name="paymethod" value="Internet Banking"> Internet Banking
+												<input type="radio" name="paymethod" value="Debit / Credit card"> Debit / Credit card <br /><br />
+												<input type="submit" value="submit" name="submit" class="btn btn-primary">
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php echo include('includes/brands-slider.php'); ?>
+			</div>
+		</div>
+		<?php include('includes/footer.php'); ?>
+		<script src="assets/js/jquery-1.11.1.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+		<script src="assets/js/owl.carousel.min.js"></script>
+		<script src="assets/js/echo.min.js"></script>
+		<script src="assets/js/jquery.easing-1.3.min.js"></script>
+		<script src="assets/js/bootstrap-slider.min.js"></script>
+		<script src="assets/js/jquery.rateit.min.js"></script>
+		<script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+		<script src="assets/js/bootstrap-select.min.js"></script>
+		<script src="assets/js/wow.min.js"></script>
+		<script src="assets/js/scripts.js"></script>
+		<script src="switchstylesheet/switchstylesheet.js"></script>
+		<script>
+			$(document).ready(function() {
+				$(".changecolor").switchstylesheet({
+					seperator: "color"
+				});
+				$('.show-theme-options').click(function() {
+					$(this).parent().toggleClass('open');
+					return false;
+				});
 			});
-		});
+			$(window).bind("load", function() {
+				$('.show-theme-options').delay(2000).trigger('click');
+			});
+		</script>
+	</body>
 
-		$(window).bind("load", function() {
-		   $('.show-theme-options').delay(2000).trigger('click');
-		});
-	</script>
-	
-
-	
-
-</body>
-</html>
+	</html>
 <?php } ?>
